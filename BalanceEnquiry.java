ba6d06e8c25231b6c357edb75e2441ed581a7af1
Abhiram Mandadi <142098886+gitabhi15@users.noreply.github.com>
2024-11-01 16:27:19 +0530

    Update BalanceEnquiry.java

--- BalanceEnquiry.java
+++ BalanceEnquiry.java
@@ -1,3 +1,15 @@
-public class BalanceEnquiry {
-    
+public abstract class BalanceEnquiry{
+    public final String PIN;
+    public float balance;
+    public BalanceEnquiry(){
+        this.balance = 5000000;
+        this.PIN = "5403";
+    }
+    public String getPIN(){
+        return this.PIN;
+    }
+    public float getBalance(){
+        return this.balance;
+    }
+    public abstract void balance_enquiry();
 }
