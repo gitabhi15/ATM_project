f5b55493a088d1e6b1fc0109c07d7306ef2a9a54
Abhiram Mandadi <142098886+gitabhi15@users.noreply.github.com>
2024-11-01 16:28:47 +0530

    Add files via upload

---
+++ HomeScreen.java
@@ -0,0 +1,11 @@
+public class HomeScreen{
+
+    public void home_page_display(){
+        System.out.println("Welcome to HFDC Bank!\nPlease select an option below to begin:");
+        System.out.println("1.Balance Enquiry\t2.Cash Withdrawl");
+        System.out.println("3.Mobile Recharge\t4.Open Fixed Deposit");
+        System.out.println("5.PIN Change\t\t6.Ultra Fast Cash");
+        System.out.println("7.Utility Bill Payment");
+    }
+    
+}
