c88b9655e2472806d803208f34640b294f0457a6
Abhiram Mandadi <142098886+gitabhi15@users.noreply.github.com>
2024-11-01 16:28:08 +0530

    Create CheckBalance.java

---
+++ CheckBalance.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+public class CheckBalance extends BalanceEnquiry{
+    
+    Scanner sc = new Scanner(System.in);
+    String dummyPin, response;
+    HomeScreen hs;
+
+    public CheckBalance(){
+        super();
+    }
+
+    public void balance_enquiry(){
+        System.out.println("Please enter your PIN:");
+        dummyPin = sc.nextLine();
+
+        if(dummyPin.equals(PIN)){
+            System.out.println("Your account balance is:");
+            System.out.println("\t\t" + balance);
+
+            try{
+                Thread.sleep(3000);
+            }
+            catch(InterruptedException e){
+                System.err.println("Oops! Something went wrong. Please try again later!" + e.getMessage());
+            }
+
+            System.out.println("Would you like to print a receipt?");
+            response = sc.nextLine();
+            
+            if(response == "Yes"){
+                System.out.println("Account Balance: " + balance);
+            }
+            else{
+                System.out.println("Thank you for your patience!");
+                
+                try{
+                    Thread.sleep(2000);
+                }
+                catch(InterruptedException e){
+                    System.err.println("Oops! Something went wrong. Please try again later!" + e.getMessage());
+                }
+
+                hs.home_page_display();
+            }
+        }
+        else{
+            System.out.println("Oops! Something went wrong. Please try again later!");
+            hs.home_page_display();
+        }
+    }
+}
