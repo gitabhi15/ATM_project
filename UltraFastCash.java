f3f483eca7786f431a6baa2ef065d14bd21612f8
Abhiram Mandadi <142098886+gitabhi15@users.noreply.github.com>
2024-11-12 12:50:42 +0530

    Update UltraFastCash.java

--- UltraFastCash.java
+++ UltraFastCash.java
@@ -1,3 +1,64 @@
-public class UltraFastCash {
+import java.util.*;
+
+public class UltraFastCash extends CashWithdrawl {
     
-}
+    Scanner sc = new Scanner(System.in);
+    HomeScreen hs;
+    float amount;
+    String dummyPin;
+
+    public void withdrawCash(){
+        System.out.println("Please enter your PIN to proceed:");
+        dummyPin = sc.nextLine();
+
+        if(dummyPin.equals(BalanceEnquiry.PIN)){
+            System.out.println("Please enter the amount you would like to withdraw: (only 500/1000/2000/5000)");
+            amount = sc.nextFloat();
+
+            while(amount % 500 == 0 && amount <= 5000){
+                BalanceEnquiry.balance -= amount;
+                break;
+            }
+
+                System.out.println("Would you like to view your balance?");
+                String response = sc.nextLine();
+
+                if(response.equalsIgnoreCase("Y")){
+                    System.out.println("Account Balance: " + BalanceEnquiry.balance);
+
+                    try{
+                        Thread.sleep(3000);
+                    }
+                    catch(InterruptedException e){
+                        System.err.println("Oops! Something went wrong. Please try again later!" + e.getMessage());
+                    }
+
+                    hs.home_page_display();
+                }
+                else{
+                    System.out.println("Thank you for using Axis Bank Services! Please wait....");
+
+                    try{
+                        Thread.sleep(3000);
+                    }
+                    catch(InterruptedException e){
+                        System.err.println("Oops! Something went wrong. Please try again later!" + e.getMessage());
+                    }
+
+                    hs.home_page_display();
+                }
+            }    
+            else{
+                System.out.println("Wrong PIN entered! Please try again.");
+
+                try{
+                    Thread.sleep(3000);
+                }
+                catch(InterruptedException e){
+                    System.err.println("Oops! Something went wrong. Please try again later!" + e.getMessage());
+                }
+
+                hs.home_page_display();
+            }
+        }
+    }
