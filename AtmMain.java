17fd584d38ab4863d6bf5b8871ae3d68a26a0a2f
Abhiram Mandadi <142098886+gitabhi15@users.noreply.github.com>
2025-01-26 15:00:29 +0530

    Update AtmMain.java

--- AtmMain.java
+++ AtmMain.java
@@ -1,8 +1,10 @@
-import java.util.*;
+import java.util.Scanner;
+import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
 
 public class AtmMain {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         HomeScreen hs = new HomeScreen();
         BalanceEnquiry balanceEnquiry = new CheckBalance();
         CashWithdrawl cashWithdrawl = new WithdrawCash();
@@ -10,36 +12,46 @@ public class AtmMain {
         UtilityBillPayment utilityBillPayment = new PayBills();
         mobilePhoneUpdate phoneUpdate = new updatePhoneNumber();
 
-        hs.home_page_display();
-        int option = sc.nextInt();
+        System.out.println("Welcome to HFDC Bank!");
+        System.out.println("Please select an option below to begin:");
+        System.out.println("1.Balance Enquiry       2.Cash Withdrawl");
+        System.out.println("3.PIN Change            4.Ultra Fast Cash");
+        System.out.println("5.Utility Bill Payment  6.Update Mobile Phone Number");
 
-        switch (option) {
-            case 1:
-                balanceEnquiry.balance_enquiry();
-                break;
-            case 2:
-                cashWithdrawl.withdrawCash();
-                break;
-            case 3:
-                // Implement Mobile Recharge functionality
-                break;
-            case 4:
-                // Implement Open Fixed Deposit functionality
-                break;
-            case 5:
-                pinChange.ChangePIN();
-                break;
-            case 6:
-                cashWithdrawl = new UltraFastCash();
-                cashWithdrawl.withdrawCash();
-                break;
-            case 7:
-                ((PayBills) utilityBillPayment).selectBillToPay();
-                break;
-            default:
-                System.out.println("Invalid option. Please try again.");
-                hs.home_page_display();
-                break;
+        try {
+            int option = scanner.nextInt();
+
+            switch (option) {
+                case 1:
+                    balanceEnquiry.balance_enquiry();
+                    break;
+                case 2:
+                    cashWithdrawl.withdrawCash();
+                    break;
+                case 3:
+                    pinChange.ChangePIN();
+                    break;
+                case 4:
+                    cashWithdrawl = new UltraFastCash();
+                    cashWithdrawl.withdrawCash();
+                    break;
+                case 5:
+                    ((PayBills) utilityBillPayment).selectBillToPay();
+                    break;
+                case 6:
+                    phoneUpdate.updateNumber();
+                    break;
+                default:
+                    System.out.println("Invalid option. Please try again.");
+                    hs.home_page_display();
+                    break;
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid input. Please enter a number.");
+        } catch (NoSuchElementException e) {
+            System.out.println("No input provided. Please enter a number.");
+        } finally {
+            scanner.close();
         }
     }
 }
