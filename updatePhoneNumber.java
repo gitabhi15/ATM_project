8f80ea08d0ecb7e049e6075edc0baff95f560098
Abhiram Mandadi <142098886+gitabhi15@users.noreply.github.com>
2025-03-08 21:38:56 +0530

    Update updatePhoneNumber.java

--- updatePhoneNumber.java
+++ updatePhoneNumber.java
@@ -3,19 +3,35 @@ import java.util.*;
 public class updatePhoneNumber extends mobilePhoneUpdate{
 
     Scanner sc = new Scanner(System.in);
-    public String phoneNumber = "9034667922";
-    HomeScreen hs;
-    String dummyPin, dummyNumber;
+    HomeScreen hs = new HomeScreen();
+    String dummyPin, dummyNumber, realPin = BankDetails.PIN;
+    String phoneNo = BankDetails.phoneNumber;
 
     public void updateNumber(){
         System.out.println("Please enter your PIN:");
         dummyPin = sc.nextLine();
 
-        if(dummyPin.equals(BalanceEnquiry.PIN)){
-            System.out.println("Please enter your new phone number:");
-            dummyNumber = sc.nextLine();
-            phoneNumber = dummyNumber;
-            System.out.println("Phone number updated successfully!");
+        if(dummyPin.equals(realPin)){
+            System.out.println("Enter your current phone number:");
+            String currentPhoneNumber = sc.nextLine();
+    
+            if (currentPhoneNumber.equals(phoneNo)) { // Compare with stored phone number
+                System.out.println("Enter your new phone number:");
+                String newPhoneNumber = sc.nextLine();
+    
+                System.out.println("Re-enter your new phone number:");
+                String confirmPhoneNumber = sc.nextLine();
+    
+                if (newPhoneNumber.equals(confirmPhoneNumber)) { // Confirm new phone number
+                    BankDetails.phoneNumber = newPhoneNumber;
+                    System.out.println("Phone number updated successfully!");
+                    hs.home_page_display();
+                } else {
+                    System.out.println("Phone number confirmation does not match. Please try again.");
+                }
+            } else {
+                System.out.println("Incorrect current phone number. Please try again.");
+            }
         }
         else{
             System.out.println("Invalid PIN. Please try again.");
