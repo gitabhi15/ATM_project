47f274df4df5821fdbd0415711d576342cec5e10
Abhiram Mandadi <142098886+gitabhi15@users.noreply.github.com>
2025-03-08 21:35:10 +0530

    Update ChangePin.java

--- ChangePin.java
+++ ChangePin.java
@@ -3,27 +3,21 @@ import java.util.*;
 public class ChangePin extends PIN_Change{
 
     Scanner sc = new Scanner(System.in);
-    HomeScreen hs;
-    String newPIN, dummyPin;
+    HomeScreen hs = new HomeScreen();
+    String newPIN, dummyPin, realPin = BankDetails.PIN;
 
     public void ChangePIN(){
         System.out.println("Please enter your current PIN:");
         dummyPin = sc.nextLine();
 
-        try{
-            Thread.sleep(3000);
-        }
-        catch(InterruptedException e){
-            System.err.println("Oops! Something went wrong. Please try again later!" + e.getMessage());
-        }
-
-        if(dummyPin.equals(BalanceEnquiry.PIN)){
+        if(dummyPin.equals(realPin)){
             System.out.println("Please enter your new PIN to proceed:");
             newPIN = sc.nextLine();
-            BalanceEnquiry.PIN = newPIN;
+            BankDetails.PIN = newPIN;
+
             System.out.println("Please re-enter your new PIN to confirm:");
             String dummyPin2 = sc.nextLine();
-            if(dummyPin2.equals(BalanceEnquiry.PIN)){
+            if(dummyPin2.equals(BankDetails.PIN)){
                 System.out.println("Thank you! Your PIN has now been updated.\nPlease wait a few seconds.....");
             }
             else{
@@ -48,5 +42,6 @@ public class ChangePin extends PIN_Change{
             System.out.println("Incorrect PIN entered! Please try again later.");
             hs.home_page_display();
         }
+        hs.home_page_display();
     }
 }
