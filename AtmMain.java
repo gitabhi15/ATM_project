4d76c1d4c44de16737a0c5e739658798f8ee93e8
Abhiram Mandadi <142098886+gitabhi15@users.noreply.github.com>
2025-03-08 21:33:40 +0530

    Update AtmMain.java

--- AtmMain.java
+++ AtmMain.java
@@ -1,57 +1,12 @@
-import java.util.Scanner;
-import java.util.InputMismatchException;
-import java.util.NoSuchElementException;
-
 public class AtmMain {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        // Welcome message
+        System.out.println("\n\t\tWelcome to the Axis Bank ATM System!\n");
+
+        // Initialize and display the home screen
         HomeScreen hs = new HomeScreen();
-        BalanceEnquiry balanceEnquiry = new CheckBalance();
-        CashWithdrawl cashWithdrawl = new WithdrawCash();
-        PIN_Change pinChange = new ChangePin();
-        UtilityBillPayment utilityBillPayment = new PayBills();
-        mobilePhoneUpdate phoneUpdate = new updatePhoneNumber();
+        hs.home_page_display(); // Start the ATM by displaying the home page
 
-        System.out.println("Welcome to HFDC Bank!");
-        System.out.println("Please select an option below to begin:");
-        System.out.println("1.Balance Enquiry       2.Cash Withdrawl");
-        System.out.println("3.PIN Change            4.Ultra Fast Cash");
-        System.out.println("5.Utility Bill Payment  6.Update Mobile Phone Number");
-
-        try {
-            int option = scanner.nextInt();
-
-            switch (option) {
-                case 1:
-                    balanceEnquiry.balance_enquiry();
-                    break;
-                case 2:
-                    cashWithdrawl.withdrawCash();
-                    break;
-                case 3:
-                    pinChange.ChangePIN();
-                    break;
-                case 4:
-                    cashWithdrawl = new UltraFastCash();
-                    cashWithdrawl.withdrawCash();
-                    break;
-                case 5:
-                    ((PayBills) utilityBillPayment).selectBillToPay();
-                    break;
-                case 6:
-                    phoneUpdate.updateNumber();
-                    break;
-                default:
-                    System.out.println("Invalid option. Please try again.");
-                    hs.home_page_display();
-                    break;
-            }
-        } catch (InputMismatchException e) {
-            System.out.println("Invalid input. Please enter a number.");
-        } catch (NoSuchElementException e) {
-            System.out.println("No input provided. Please enter a number.");
-        } finally {
-            scanner.close();
-        }
+        // The rest of the flow control will be handled by the HomeScreen class.
     }
 }
