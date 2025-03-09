28a408e053fe16f37998a56f96d405067662b279
Abhiram Mandadi <142098886+gitabhi15@users.noreply.github.com>
2025-03-09 10:27:17 +0530

    Update UtilityBillPayment.java

--- UtilityBillPayment.java
+++ UtilityBillPayment.java
@@ -1,6 +1,6 @@
 public abstract class UtilityBillPayment {
-    public abstract float payWaterBill();
-    public abstract float payMobileBill();
-    public abstract float payElectricityBill();
-    public abstract float payCreditCardBill();
+    public abstract void payWaterBill(float waterBill);
+    public abstract void payMobileBill(float mobileBill);
+    public abstract void payElectricityBill(float electricityBill);
+    public abstract void payCreditCardBill(float creditCardBill);
 }
